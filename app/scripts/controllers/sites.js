5b37970ed2bfeefefd7794b30c6d9c0a503a6369
Rylan Bowers <rylanb@gmail.com>
2015-06-07 14:56:26 -0600

    Cleaning up indentation / spacing in sites.js.

--- app/scripts/controllers/sites.js
+++ app/scripts/controllers/sites.js
@@ -9,18 +9,20 @@
  */
 angular.module('ersApp')
   .controller('SitesCtrl', function ($scope,$http) {
-   $scope.config = {
+
+  $scope.config = {
     itemsPerPage: 10
 	}
-    $scope.state_abb = ["MI","SD","WA","WI","AZ","IL","NH","NC","KS","MO","AR",
-                        "NV","DC","ID","NE","PA","HI","UT","VT","DE","RI","OK","LA",
-                        "MT","TN","MD","FL","VA","MN","NJ","OH","CA","ND","ME","IN",
-                        "TX","OR","WY","AL","IA","MS","KY","NM","GA","CO","MA","CT",
-                        "NY","SC","AK","WV","AA","AE","AP"];
+  $scope.state_abb = ["MI","SD","WA","WI","AZ","IL","NH","NC","KS","MO","AR",
+                      "NV","DC","ID","NE","PA","HI","UT","VT","DE","RI","OK","LA",
+                      "MT","TN","MD","FL","VA","MN","NJ","OH","CA","ND","ME","IN",
+                      "TX","OR","WY","AL","IA","MS","KY","NM","GA","CO","MA","CT",
+                      "NY","SC","AK","WV","AA","AE","AP"];
   $scope.selected = 1;
   $scope.isActive = function(index) {
-        return $scope.selected === index;
- };
+    return $scope.selected === index;
+  };
+
   $scope.gotopage = function(index){
     $scope.selected = index;
 
@@ -36,19 +38,7 @@ angular.module('ersApp')
     }).error(function(){
         alert("error");
     })
-  } 
-  $http({
-        method: 'GET',
-        url: 'http://54.68.73.69/api/v1/sites',
-         headers: {
-        'Content-type': 'application/json'
-        }
-     }).success(function(data){
-        $scope.sites= data;
-        $scope.siteList = data.sites;
-    }).error(function(){
-        alert("error");
-    })
+  }
   $scope.pagerange = function(min, max, step){
     step = step || 1;
     var input = [];
@@ -56,6 +46,18 @@ angular.module('ersApp')
     return input;
   }
 
-  
-  });
+  $http({
+    method: 'GET',
+    url: 'http://54.68.73.69/api/v1/sites',
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  .success(function(data){
+    $scope.sites = data;
+    $scope.siteList = data.sites;
+  }).error(function(){
+    alert("error");
+  })
+});
 
