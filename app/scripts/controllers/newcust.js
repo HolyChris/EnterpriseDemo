e07fc8269ce56123844e08c9a3807d5dfc944376
Rodolfo <rodolfo.leyes@toptal.com>
2015-06-12 20:53:18 +0000

    Phone number already taken error returned by backend shown in view. Added site creation as a next step when creating Customer. Site creation validations works for state fields as these are the only one errors returned by backend

--- app/scripts/controllers/newcust.js
+++ app/scripts/controllers/newcust.js
@@ -25,4 +25,34 @@ $scope.user={}
 
     })
   }
-  });
+  
+  $scope.newCustomerThenSite = function(user,phone_number){
+    $http({
+        method: 'POST',
+        url: ENV.apiEndpoint + '/api/v1/customers?phone_numbers_attributes[0][number]='+phone_number.number+'&phone_numbers_attributes[0][num_type]=1&phone_numbers_attributes[0][primary]=true',
+        params: user,
+         headers: {
+        'Content-type': 'application/json'
+        }
+     }).success(function(data){
+        $scope.customers= data;
+        
+        var message = 'You have succesfully created a customer.';
+        Flash.create('success', message);
+        
+        $scope.Flash=Flash;
+        $scope.custList = data.customers;
+        
+        var url_params= {
+            customerId: data.customer.id
+        };
+        $location.path("/sites/new").search(url_params);
+    }).error(function(data){
+        $scope.errors= data.errors;
+        Flash.create('danger', "Customer was not created see errors below");
+
+    })
+  }
+
+  
+});
--- app/scripts/controllers/newsite.js
+++ app/scripts/controllers/newsite.js
@@ -3,7 +3,14 @@ angular.module('ersApp')
 .run(function($http) {
   $http.defaults.headers.common= { 'X-Auth-Token' : 'D2EdWKgbs8cq9PHyLhrA' };
 })
-  .controller('NewSiteCtrl', function ($scope,$http,ENV) {
+  .controller('NewSiteCtrl', function ($scope,$location,$http,ENV,Flash) {
+    
+  //Here we find out if the url is passing a customerId
+  if ($location.search().customerId)
+  {
+    $scope.customer_id=$location.search().customerId;
+  }
+  
   $scope.flag=0;
   $scope.siteAddress={};
   $scope.billAddress={};
@@ -27,13 +34,16 @@ angular.module('ersApp')
   $scope.newSite = function(siteAddress,siteDetail,billAddress){
     $http({
         method: 'POST',
-        url: ENV.apiEndpoint + '/api/v1/sites?customer_id=109&name='+siteDetail.name +'&contact_name='+siteDetail.contact_name+'&contact_phone='+siteDetail.contact_phone+'&manage_ids[]=1&manage_ids[]=2&source='+siteDetail.source+'&source_info='+siteDetail.source_info+'&status='+siteDetail.status+'&damage='+siteDetail.damage+'&address_attributes[address1]='+siteAddress.address1+'&address_attributes[address2]='+siteAddress.address2+'&address_attributes[city]='+siteAddress.address2+'&address_attributes[state_id]='+siteAddress.state_id+'&address_attributes[zipcode]='+siteAddress.zipcode+'&bill_addr_same_as_addr='+$scope.flag+'&bill_address_attributes[address1]='+billAddress.address1+'&bill_address_attributes[address2]='+billAddress.address2+'&bill_address_attributes[city]='+billAddress.city+'&bill_address_attributes[state_id]='+billAddress.state_id+'&bill_address_attributes[zipcode]='+billAddress.zipcode,
+        url: ENV.apiEndpoint + '/api/v1/sites?customer_id=' + $scope.customer_id + '&name='+siteDetail.name +'&contact_name='+siteDetail.contact_name+'&contact_phone='+siteDetail.contact_phone+'&manage_ids[]=1&manage_ids[]=2&source='+siteDetail.source+'&source_info='+siteDetail.source_info+'&status='+siteDetail.status+'&damage='+siteDetail.damage+'&address_attributes[address1]='+siteAddress.address1+'&address_attributes[address2]='+siteAddress.address2+'&address_attributes[city]='+siteAddress.address2+'&address_attributes[state_id]='+siteAddress.state_id+'&address_attributes[zipcode]='+siteAddress.zipcode+'&bill_addr_same_as_addr='+$scope.flag+'&bill_address_attributes[address1]='+billAddress.address1+'&bill_address_attributes[address2]='+billAddress.address2+'&bill_address_attributes[city]='+billAddress.city+'&bill_address_attributes[state_id]='+billAddress.state_id+'&bill_address_attributes[zipcode]='+billAddress.zipcode,
          headers: {
         'Content-type': 'application/json'
         }
      }).success(function(data){
-    }).error(function(){
-        alert("error");
+       Flash.create('success', 'Site created succesfully');
+       $location.path("/sites")
+    }).error(function(data){
+       $scope.errors=data.errors;
+       Flash.create('danger', 'Site was not created');
     })
   };
   });
