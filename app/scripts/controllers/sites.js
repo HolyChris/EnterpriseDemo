9d691594351233f365afd8f9a86badb313bd708e
Rodolfo <rodolfo.leyes@toptal.com>
2015-06-17 02:36:15 +0000

    Site search querying backend is now functional

--- app/scripts/controllers/sites.js
+++ app/scripts/controllers/sites.js
@@ -48,6 +48,74 @@ angular.module('ersApp')
     for (var i = min; i <= max; i += step) input.push(i);
     return input;
   };
+  
+  $scope.findSites = function(search_by_manager,search_by_name,search_by_address,search_by_city,search_by_state_id,search_by_zipcode){
+    
+    
+    var query_params=[];
+    
+    if (!angular.isUndefined(search_by_manager) && search_by_manager.trim()!="" )
+    {
+      query_params.push('q[managers_email_eq]=' + search_by_manager);
+    }
+    
+    if (!angular.isUndefined(search_by_name) && search_by_name.trim()!="" )
+    {
+      query_params.push('q[name_cont]=' + search_by_name);
+    }
+    
+    if (!angular.isUndefined(search_by_address) && search_by_address.trim()!="" )
+    {
+      query_params.push('q[address_address1_cont]=' + search_by_address);
+    }
+    
+    if (!angular.isUndefined(search_by_city) && search_by_city.trim()!="" )
+    {
+      query_params.push('q[address_city_cont]=' + search_by_city);
+    }
+    
+    if (!angular.isUndefined(search_by_state_id) && search_by_state_id.trim()!="" )
+    {
+      query_params.push('q[address_state_id_eq]=' + search_by_state_id);
+    }
+    
+    if (!angular.isUndefined(search_by_zipcode) && search_by_zipcode.trim()!="" )
+    {
+      query_params.push('q[address_zipcode_eq]=' + search_by_zipcode);
+    }   
+    
+    var query_string=""
+    var i=0;
+    for (i=0;i<query_params.length;i++)
+    {
+      if (i>0)
+      {
+        query_string=query_string + "&";
+      }
+      query_string+= query_params[i];
+    }
+
+    if (query_string!="")
+    {
+      query_string="?" + query_string;
+    }
+    
+    
+    $http({
+      method: 'GET',
+      url: ENV.apiEndpoint + '/api/v1/sites' + query_string,
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .success(function(data){
+      $scope.sites = data;
+      $scope.siteList = data.sites;
+    }).error(function(){
+      alert("error");
+    })
+    
+  };
 
   $http({
     method: 'GET',
