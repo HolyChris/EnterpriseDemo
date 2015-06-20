746e138e9eccd0553fdb2d803f34ac8bbbf6e39b
Rodolfo <rodolfo.leyes@toptal.com>
2015-06-20 11:51:30 +0000

    Customer search now functional

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -55,5 +55,55 @@ angular.module('ersApp')
   }
 
 
+  $scope.findCustomers = function(search_by_firstname,search_by_lastname,search_by_email){
+    var query_params=[];
+    
+    if (!angular.isUndefined(search_by_firstname) && search_by_firstname.trim()!="" )
+    {
+      query_params.push('q[firstname_cont]=' + search_by_firstname);
+    }
+    
+    if (!angular.isUndefined(search_by_lastname) && search_by_lastname.trim()!="" )
+    {
+      query_params.push('q[lastname_cont]=' + search_by_lastname);
+    }
+    
+    if (!angular.isUndefined(search_by_email) && search_by_email.trim()!="" )
+    {
+      query_params.push('q[email_eq]=' + search_by_email);
+    }
+    
+    var query_string=""
+    var i=0;
+    for (i=0;i<query_params.length;i++)
+    {
+      if (i>0)
+      {
+        query_string=query_string + "&";
+      }
+      query_string+= query_params[i];
+    }
+
+    if (query_string!="")
+    {
+      query_string="?" + query_string;
+    }
+    
+    
+    $http({
+      method: 'GET',
+      url: ENV.apiEndpoint + '/api/v1/customers' + query_string,
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .success(function(data){
+      $scope.customers= data;
+      $scope.custList = data.customers;
+    }).error(function(){
+      alert("error");
+    })
+    
+  };
   });
 
