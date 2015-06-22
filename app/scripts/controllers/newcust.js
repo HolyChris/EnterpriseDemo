b252840fede59cea10a5e4c77e7a8a0413e2ad3b
Jeff Shamley <jshamley@comcast.net>
2015-06-22 12:41:19 -0600

    change company name in header | missing fields for customer creation

--- app/scripts/controllers/newcust.js
+++ app/scripts/controllers/newcust.js
@@ -1,17 +1,46 @@
 angular.module('ersApp')
   .controller('NewCustomerCtrl', function ($scope,$http, $location,ENV,Flash) {
-$scope.user={}
-  $scope.phone_number={}
-  $scope.newCustomer = function(user,phone_number){
+  
+  $scope.user = {};
+  $scope.phone_number = [];
+  
+  $scope.addPhone = function() {
+    var newItemNo = $scope.phone_number.length + 1;
+    $scope.phone_number.push({
+      'type': '',
+      'number': '',
+      'primary': false, // only 1 primary can exist
+      'remove': false, // to remove from backend
+      'newPhone': true // new phone added from front end
+    });
+  }
+
+  $scope.removePhone = function(index) {
+    $scope.phone_number.splice(index, 1);
+  }
+
+  function phonePrepare() {
+    var urlFragment = '';
+    for (var i = 0; i < $scope.phone_number.length; i++) {
+      if ($scope.phone_number[i].number.length > 0) {
+        urlFragment += 'phone_numbers_attributes[' + i + '][number]=' + $scope.phone_number[i].number + '&phone_numbers_attributes[' + i + '][num_type]=' + $scope.phone_number[i].type + '&phone_numbers_attributes[' + i + '][primary]=' + $scope.phone_number[i].primary + '&';
+      }
+    }
+    return urlFragment;
+  }
+
+  $scope.newCustomer = function(user) {
+    var phoneUrlFragment = phonePrepare();
+
     $http({
         method: 'POST',
-        url: ENV.apiEndpoint + '/api/v1/customers?phone_numbers_attributes[0][number]='+phone_number.number+'&phone_numbers_attributes[0][num_type]=1&phone_numbers_attributes[0][primary]=true',
+        url: ENV.apiEndpoint + '/api/v1/customers?' + phoneUrlFragment,
         params: user,
          headers: {
         'Content-type': 'application/json'
         }
      }).success(function(data){
-        $scope.customers= data;
+        $scope.customers = data;
         
         var message = 'You have succesfully created a customer. <a href="#" class="alert-link">Click here to create a site </a> for this customer.';
         Flash.create('success', message);
@@ -20,22 +49,24 @@ $scope.user={}
         $scope.custList = data.customers;
         $location.path("/customers/overview")
     }).error(function(data){
-        $scope.errors= data.errors;
+        $scope.errors = data.errors;
         Flash.create('danger', "Customer was not created see errors below");
-
+        console.log($scope.errors);
     })
   }
   
-  $scope.newCustomerThenSite = function(user,phone_number){
+  $scope.newCustomerThenSite = function(user) {
+    var phoneUrlFragment = phonePrepare();
+
     $http({
         method: 'POST',
-        url: ENV.apiEndpoint + '/api/v1/customers?phone_numbers_attributes[0][number]='+phone_number.number+'&phone_numbers_attributes[0][num_type]=1&phone_numbers_attributes[0][primary]=true',
+        url: ENV.apiEndpoint + '/api/v1/customers?' + phoneUrlFragment,
         params: user,
          headers: {
         'Content-type': 'application/json'
         }
      }).success(function(data){
-        $scope.customers= data;
+        $scope.customers = data;
         
         var message = 'You have succesfully created a customer.';
         Flash.create('success', message);
@@ -48,9 +79,9 @@ $scope.user={}
         };
         $location.path("/sites/new").search(url_params);
     }).error(function(data){
-        $scope.errors= data.errors;
+        $scope.errors = data.errors;
         Flash.create('danger', "Customer was not created see errors below");
-
+        console.log(data.errors);
     })
   }
 
