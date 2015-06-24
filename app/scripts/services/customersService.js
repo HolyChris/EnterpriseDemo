cfa2a4e3e2a3c6ea00bf0aee8eeb25d00c5bdbd2
Rodolfo <rodolfo.leyes@toptal.com>
2015-06-24 13:58:30 +0000

    Corrected customer linking to when coming from new customer

--- app/scripts/services/customersService.js
+++ app/scripts/services/customersService.js
@@ -1,7 +1,7 @@
 var sitesService = angular.module('customersService', ['ngResource']);
 
 sitesService.factory('Customer', function($resource, ENV) {
-  return $resource(ENV.apiEndpoint + '/api/v1/customers', {}, {
+  return $resource(ENV.apiEndpoint + '/api/v1/customers/:customerId', {customerId:'@id'}, {
     query: {method: "GET", isArray: false},
     search: {method: "GET", isArray: false}
   });
