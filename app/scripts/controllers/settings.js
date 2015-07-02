d3adf4463a2ed1da619a5eac14420d975084c553
Laureano Arcanio <larcanio@gmail.com>
2015-07-02 01:22:46 -0300

    Implement settings controler with validations

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -1,5 +1,32 @@
 angular.module('ersApp')
-  .controller('SettingsCtrl', function($scope) {
+  .controller('SettingsCtrl', function($scope, User, Flash) {
+    $scope.data = {};
+    // This is not ideall but it's the only way to retrieve userd data
+    // we dont have a client id in frontend
+    var user = User.save(function(data) {
+      $scope.data.fullname = data.user.fullname;
+      $scope.data.email = data.user.email;
+    })
 
-    console.log("In Settings")
+    $scope.changePassword = function() {
+      // sanitization so it doesn't send empty values
+      var params = {};
+      angular.forEach($scope.data, function(value, key) {
+        if (value !== "") {
+          params[key] = value;
+        }
+      })
+      User.save(params, function(data) {
+        if (data.errors) {
+          $scope.errors = {};
+          console.log("has errors", data.errors)
+
+          angular.forEach(data.errors, function(value, key) {
+            $scope.errors[key] = value[0];
+          })
+        
+          console.log($scope.errors)
+        }
+      });
+    }
   })
