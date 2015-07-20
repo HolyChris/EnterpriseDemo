ffd9228e64812df48099ef66596d14584dfcda70
Laureano Arcanio <larcanio@gmail.com>
2015-07-20 14:22:18 -0300

    Monkey patch new customer to fix validations

--- app/scripts/controllers/newsite.js
+++ app/scripts/controllers/newsite.js
@@ -56,22 +56,31 @@ angular.module('ersApp')
   $scope.newSite = function(siteAddress,siteDetail,billAddress) {
     var manageIds = manage_ids.length ? manage_ids.join('') : '';
     // re-factor this to use service.
-    $http({
-        method: 'POST',
-        url: ENV.apiEndpoint + '/api/v1/sites?customer_id=' + $scope.customer.id + '&name='+siteDetail.name + manageIds + '&contact_name='+siteDetail.contact_name+'&contact_phone='+siteDetail.contact_phone+'&manage_ids[]=1&manage_ids[]=2&source='+siteDetail.source+'&source_info='+siteDetail.source_info+'&status='+siteDetail.status+'&damage='+siteDetail.damage+'&address_attributes[address1]='+siteAddress.address1+'&address_attributes[address2]='+siteAddress.address2+'&address_attributes[city]='+siteAddress.address2+'&address_attributes[state_id]='+siteAddress.state_id+'&address_attributes[zipcode]='+siteAddress.zipcode+'&bill_addr_same_as_addr='+$scope.flag+'&bill_address_attributes[address1]='+billAddress.address1+'&bill_address_attributes[address2]='+billAddress.address2+'&bill_address_attributes[city]='+billAddress.city+'&bill_address_attributes[state_id]='+billAddress.state_id+'&bill_address_attributes[zipcode]='+billAddress.zipcode,
-        headers: {
-          'Content-type': 'application/json'
-        }
-     }).success(function(data){
-       Flash.create('success', 'Site created succesfully');
 
-        $state.go("project.overview",{projectId: data.site.id})
-        
-    }).error(function(data){
-       $scope.errors=data.errors;
-       Flash.create('danger', 'Site was not created');
-       console.log(data.errors);
-    })
+    if ($scope.customer) {
+      $http({
+          method: 'POST',
+          url: ENV.apiEndpoint + '/api/v1/sites?customer_id=' + $scope.customer.id + '&name='+siteDetail.name + manageIds + '&contact_name='+siteDetail.contact_name+'&contact_phone='+siteDetail.contact_phone+'&manage_ids[]=1&manage_ids[]=2&source='+siteDetail.source+'&source_info='+siteDetail.source_info+'&status='+siteDetail.status+'&damage='+siteDetail.damage+'&address_attributes[address1]='+siteAddress.address1+'&address_attributes[address2]='+siteAddress.address2+'&address_attributes[city]='+siteAddress.address2+'&address_attributes[state_id]='+siteAddress.state_id+'&address_attributes[zipcode]='+siteAddress.zipcode+'&bill_addr_same_as_addr='+$scope.flag+'&bill_address_attributes[address1]='+billAddress.address1+'&bill_address_attributes[address2]='+billAddress.address2+'&bill_address_attributes[city]='+billAddress.city+'&bill_address_attributes[state_id]='+billAddress.state_id+'&bill_address_attributes[zipcode]='+billAddress.zipcode,
+          headers: {
+            'Content-type': 'application/json'
+          }
+       }).success(function(data){
+        if (data.errors) {
+          Flash.create('danger', 'Please fill in required data');
+        } else {
+          Flash.create('success', 'Site created succesfully');
+          $state.go("project.overview",{projectId: data.site.id})
+        }
+      }).error(function(data){
+         $scope.errors=data.errors;
+         Flash.create('danger', 'Site was not created');
+         console.log(data.errors);
+      });
+    } else {
+      Flash.create('danger', 'You must link the site to a customer before saving');
+    }
+
+
   };
 
   $scope.managersArray = Managers.query();
