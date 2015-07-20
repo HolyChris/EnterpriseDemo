bda78f314bf6e44ae9bedc25b3ab7524bf25a809
Jeff Shamley <jshamley@comcast.net>
2015-07-20 15:02:33 -0600

    config file

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,11 +1,5 @@
 "use strict";
 
  angular.module('config', [])
-
-<<<<<<< HEAD
 .constant('ENV', {name:'production',apiEndpoint:'https://eco-roof.herokuapp.com'})
-=======
-.constant('ENV', {name:'production',apiEndpoint:'http://54.68.73.69'})
->>>>>>> adding config
-
 ;
