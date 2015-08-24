7c467d912a6236ad0455c4df6a98ab10f88c981a
Jeff Shamley <jshamley@comcast.net>
2015-08-24 13:08:12 -0600

    fixing gitignore

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,6 +2,10 @@
 
  angular.module('config', [])
 
+<<<<<<< HEAD
 .constant('ENV', {name:'production',apiEndpoint:'https://eco-roof.herokuapp.com'})
+=======
+.constant('ENV', {name:'production',apiEndpoint:'http://54.68.73.69'})
+>>>>>>> adding config
 
 ;
