6ed2f76983ef19d14ba2865bba24eb5bbd73ce49
rodley82 <rodolfo.leyes@toptal.com>
2015-10-15 16:19:52 -0300

    Enabling the frontend changes for thumbnail generation

--- app/scripts/controllers/assets.js
+++ app/scripts/controllers/assets.js
@@ -318,9 +318,7 @@ angular.module('ersApp')
             title: value.title,
             file_name: value.attachments[0].file_name,
             url: value.attachments[0].url,
-            //TODO replace to thumbnail_url when working properly
-            //thumbnailUrl: value.attachments[0].thumbnail_url,
-            thumbnailUrl: value.attachments[0].url,
+            thumbnailUrl: value.attachments[0].thumbnail_url,
             doc_type: value.doc_type,
             notes: value.notes,
             stage: value.stage,
